a9c8ebdb49b59e33f9c165904c25cf5ead73017b
Xinyuan-Liang <77651981+Xinyuan-Liang@users.noreply.github.com>
2023-05-20 21:16:20 +0800

    Add files via upload

---
+++ code/GAM.R
@@ -0,0 +1,35 @@
+# This code was used to fit the maturation curves of nodal CT with age
+
+library(mgcv)
+Data <- read.csv("F:/CBDP/521/521.csv") 
+Measures <- read.csv("F:/CBDP/521/CT/C500/mixed_model/cortical_thickness.csv", header = FALSE) # header = TRUE means that the first row of values in the .csv is set as header information (column names)
+
+scan_age = Data$scan_age
+Data$sub_id <- as.factor(Data$sub_id)
+subject=Data$sub_id
+Data$sex <- as.factor(Data$sex)
+sex=Data$sex
+
+age_term_all<-matrix(NA,nrow=521,ncol=1000)
+Pvalue_all <- matrix(NA,nrow=1000,ncol=1)
+# outpath = 'F:/data/CBDP/results/'
+for(roi in 1:1000){
+  Data$Measure <-(Measures[,roi]) 
+  Data$Measure <- as.numeric(Data$Measure)
+  Measure=Data$Measure
+  fit_G4 <- gam(Measure ~ s(scan_age, bs="tp", k=3) + sex + s(subject, bs="re"), data=Data, method="REML")
+  # save(fit_G4,file=paste0(outpath,'fit_G',roi,'.Rdata'))
+  
+  summary_model <- summary(fit_G4)
+  Pvalue_all[roi,1] = summary_model$s.table[1,4]
+  age_term = predict(fit_G4, type = 'terms')[, 's(scan_age)']
+  age_term_all[,roi]=age_term
+ 
+}
+write.table(age_term_all,"F:/data/CBDP/results/GAM_CT_age.csv",row.names=F, col.names=F)
+write.table(Pvalue_all,"F:/data/CBDP/results/Pvalue.csv",row.names=F, col.names=F)
+
+
+
+
+
